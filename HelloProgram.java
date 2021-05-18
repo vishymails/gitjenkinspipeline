ce54e5c09f68716bbdd7af4744bcfcd4ad47ef42
vishymails <vishymails@gmail.com>
2021-05-18 16:45:16 +0530

    Update HelloProgram.java

--- HelloProgram.java
+++ HelloProgram.java
@@ -1,6 +1,6 @@
 public class HelloProgram {
     public static void main(String[] s){
-        System.out.println("Loop based execution - CICD Demo");
+        System.out.println("Loop based execution - CICD Demo- FOR MY MAY BATCH ");
 
         for(int i=0; i<50;i++){
             System.out.println(i);
