affb259b5371b7a105643239ea6cf75994f319e5
vishymails <vishymails@gmail.com>
2021-05-18 16:41:30 +0530

    Create HelloProgram.java

---
+++ HelloProgram.java
@@ -0,0 +1,9 @@
+public class HelloProgram {
+    public static void main(String[] s){
+        System.out.println("Loop based execution - CICD Demo");
+
+        for(int i=0; i<50;i++){
+            System.out.println(i);
+        }
+    }
+}
